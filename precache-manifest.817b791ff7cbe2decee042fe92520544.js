a9fc2e3732e6ef4f055d28edbce7dd17ea61fdb5
rajeev <rajeevmaurya9794280914@gmail.com>
2020-10-18 11:36:28 +0530

    Updates

---
+++ precache-manifest.817b791ff7cbe2decee042fe92520544.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad3afbb59d593aac9baed5acff4da5a6",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "4f78679ccb8fb7859cf7",
+    "url": "/todoApp/static/css/main.c1d69888.chunk.css"
+  },
+  {
+    "revision": "b38cd5d1cb3c9a3480aa",
+    "url": "/todoApp/static/js/2.134f5225.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/todoApp/static/js/2.134f5225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f78679ccb8fb7859cf7",
+    "url": "/todoApp/static/js/main.5d34f531.chunk.js"
+  },
+  {
+    "revision": "df7629b979bd038184bb",
+    "url": "/todoApp/static/js/runtime-main.29991ed7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
